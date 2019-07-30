4c70f21f9966783749e810500e0077f09eabd644
Sergio Zamudio <szamu5@hotmail.com>
2019-07-30 14:47:18 -0500

    feature: convert decimals in to hexadecimal and octal
    add function print_octal and print_hex

--- print_opb.c
+++ print_opb.c
@@ -6,17 +6,123 @@
  */
 int print_octal(va_list value)
 {
-	int i, dec = 1, oct = 0;
+	unsigned int data;
+	int x, mask, flag = 0, cont = 0;
 
-	i = va_arg(value, int);
-
-	while (i > 0)
+	data = va_arg(value, unsigned int);
+	if (data == 0)
 	{
-		oct = oct + ((i % 8) * dec);
-		dec = dec * 10;
-		i = i / 8;
+		_putchar('0');
+		return (1);
 	}
-	return (oct);
+	for (x = 30; x >= 0; x = x - 3)
+	{
+		mask = (data >> x & 0x7);
+		if (mask)
+			flag = 1;
+		if (flag)
+		{
+			_putchar(mask + '0');
+			cont++;
+		}
+	}
+	return (cont);
+}
+/**
+ * print_pointer - convert and print
+ * @value: parameter
+ * Return: lenthg of parameter
+ */
+int print_pointer(va_list value)
+{
+	unsigned int data;
+	int x, mask, flag = 0, cont = 0;
+	char hex[] = {"0123456789abcdef"};
+	char num;
+
+	data = va_arg(value, unsigned int);
+	if (data == 0)
+	{
+		_putchar('0');
+		return (1);
+	}
+	for (x = (sizeof(void *) * 8); x >= 0; x = x - 4)
+	{
+		mask = (data >> x & 0xFF);
+		if (mask)
+			flag = 1;
+		if (flag)
+		{
+			num = hex[mask];
+			_putchar(num);
+			cont++;
+		}
+	}
+	return (cont);
+}
+/**
+ * print_hexa - convert and print hexadecimal numbers
+ * @value: parameter
+ * Return: lenthg of parameter
+ */
+int print_hexa(va_list value)
+{
+	unsigned int data;
+	int x, mask, flag = 0, cont = 0;
+	char hex[] = {"0123456789abcdef"};
+	char num;
+
+	data = va_arg(value, unsigned int);
+	if (data == 0)
+	{
+		_putchar('0');
+		return (1);
+	}
+	for (x = 28; x >= 0; x = x - 4)
+	{
+		mask = (data >> x & 0x0F);
+		if (mask)
+			flag = 1;
+		if (flag)
+		{
+			num = hex[mask];
+			_putchar(num);
+			cont++;
+		}
+	}
+	return (cont);
+}
+/**
+ * print_HEXA - convert and print in upercase hexadecimal numbers
+ * @value: parameter
+ * Return: lenthg of parameter
+ */
+int print_HEXA(va_list value)
+{
+	unsigned int data;
+	int x, mask, flag = 0, cont = 0;
+	char hex[] = {"0123456789ABCDEF"};
+	char num;
+
+	data = va_arg(value, unsigned int);
+	if (data == 0)
+	{
+		_putchar('0');
+		return (1);
+	}
+	for (x = 28; x >= 0; x = x - 4)
+	{
+		mask = (data >> x & 0x0F);
+		if (mask)
+			flag = 1;
+		if (flag)
+		{
+			num = hex[mask];
+			_putchar(num);
+			cont++;
+		}
+	}
+	return (cont);
 }
 /**
  * print_bin - convert and print bin
