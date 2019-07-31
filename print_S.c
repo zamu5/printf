ad145a7572a6dc192a24db9593c708b6df6e100b
Sergio Zamudio <szamu5@hotmail.com>
2019-07-30 19:59:20 -0500

    fix: change the uppercase to lowercase.

--- print_S.c
+++ print_S.c
@@ -7,8 +7,9 @@
 int print_string_S(va_list value)
 {
 	char *data = va_arg(value, char*);
-	int i, cont = 0, env;
+	int i, cont, env;
 
+	cont = 0;
 	if (data == NULL)
 		data = "(null)";
 	for (i = 0; data[i] != '\0'; i++)
